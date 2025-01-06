ee3230cc0e6793e6093514d1ed8fd99399d4c978
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-06 23:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let p = document.getElementById('status');
+let btn = document.getElementById('enterBtn');
+
+function changContent(){
+	
+	p.innerHTML = "<h1>Entered Metaverse</h1>";
+}
+
+btn.addEventListener('click', changContent);
